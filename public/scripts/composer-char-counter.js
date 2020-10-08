94a6502109e1ae26e10f12e98bb0b66b6b1c7ada
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-10-08 12:18:10 -0400

    add

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#new-tweet-area').keydown(function(){
+        var max = 140;
+        var len = $(this).val().length;
+        $('.counter').text(max - len);
+        if (len >= max) {
+          $('.counter').addClass('red');
+        }
+    });
+    
+    $(characterCount).keyup(updateCount);
+    $(characterCount).mouseup(updateCount);
+    $(characterCount).mouseout(updateCount);
+    $(characterCount).change(updateCount);
+  });
